dcdc1ce353d63ec78f5c7d6b5ea5139792a6a2eb
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-04-23 00:51:53 +0300

    Созданы 4 окна: 1) с кнопкой "PHARMACY"; 2) с кнопкой "SURFACE AREA"; 3) с кнопкой "CALCULATE"; 4) с результатом "100". Между окнами настроена навигация по системной кнопке "Back" и по кнопке в верхнем меню слева. Так же создано всплывающее окно "Bottom sheet" с текстом "INFORMATION".

--- screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/navigator/AppScreens.kt
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/navigator/AppScreens.kt
@@ -3,7 +3,8 @@ package com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 interface AppScreens {
-//    fun firstScreen(): FragmentScreen
-//    fun secondScreen(): FragmentScreen
-//    fun thirdScreen(): FragmentScreen
+    fun pharmacyScreen(): FragmentScreen
+    fun pharmacySurfaceScreen(): FragmentScreen
+    fun pharmacySurfaceCalculateScreen(): FragmentScreen
+    fun pharmacySurfaceCalculateResultScreen(): FragmentScreen
 }
--- screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/navigator/AppScreensImpl.kt
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/navigator/AppScreensImpl.kt
@@ -1,17 +1,25 @@
 package com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator
 
 import com.github.terrakok.cicerone.androidx.FragmentScreen
+import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.PharmacyFragment
+import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.PharmacySurfaceCalculateFragment
+import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.PharmacySurfaceCalculateResultFragment
+import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.PharmacySurfaceFragment
 
 class AppScreensImpl: AppScreens {
-//    override fun firstScreen() = FragmentScreen {
-//        FirstFragment.newInstance()
-//    }
-//
-//    override fun secondScreen() = FragmentScreen {
-//        SecondFragment.newInstance()
-//    }
-//
-//    override fun thirdScreen() = FragmentScreen {
-//        ThirdFragment.newInstance()
-//    }
+    override fun pharmacyScreen() = FragmentScreen {
+        PharmacyFragment.newInstance()
+    }
+
+    override fun pharmacySurfaceScreen() = FragmentScreen {
+        PharmacySurfaceFragment.newInstance()
+    }
+
+    override fun pharmacySurfaceCalculateScreen() = FragmentScreen {
+        PharmacySurfaceCalculateFragment.newInstance()
+    }
+
+    override fun pharmacySurfaceCalculateResultScreen() = FragmentScreen {
+        PharmacySurfaceCalculateResultFragment.newInstance()
+    }
 }
