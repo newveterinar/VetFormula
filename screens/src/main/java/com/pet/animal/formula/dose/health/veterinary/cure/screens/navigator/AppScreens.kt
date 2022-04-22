0b87184e7951d6429dffb969bb30d67491a21a95
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-04-22 18:17:26 +0300

    Подключена библиотека "Koin" в проект. Созданы в коине следующие сущности: 1. Scope для MainActivity. 2. Класс "Cicerone". 3. Класс "Router". 4. Класс "NavigatorHolder". 5. Класс "AppScreensImpl".
    Скорректирована связь между модулями:
    теперь связи между модулями реализуются через
    api project(":НАЗВАНИЕ МОДУЛЯ"), а не через implementation project(":НАЗВАНИЕ МОДУЛЯ"), что позволяет напрямую обращаться к классам в самых удалённых модулях.
     Из всех модулей убраны закоменнтированные библиотеки "Dagger" и "Moxy".

---
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/navigator/AppScreens.kt
@@ -0,0 +1,9 @@
+package com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator
+
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+
+interface AppScreens {
+//    fun firstScreen(): FragmentScreen
+//    fun secondScreen(): FragmentScreen
+//    fun thirdScreen(): FragmentScreen
+}
---
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/navigator/AppScreensImpl.kt
@@ -0,0 +1,17 @@
+package com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator
+
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+
+class AppScreensImpl: AppScreens {
+//    override fun firstScreen() = FragmentScreen {
+//        FirstFragment.newInstance()
+//    }
+//
+//    override fun secondScreen() = FragmentScreen {
+//        SecondFragment.newInstance()
+//    }
+//
+//    override fun thirdScreen() = FragmentScreen {
+//        ThirdFragment.newInstance()
+//    }
+}
