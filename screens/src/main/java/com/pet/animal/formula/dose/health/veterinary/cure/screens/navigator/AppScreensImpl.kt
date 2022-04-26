bb9f93a7c7caa67873932f09e190901d185cafae
AVKhakhalin <78661461+AVKhakhalin@users.noreply.github.com>
2022-04-26 11:36:09 +0300

    Исправлены замечания с код-ревью на лекции 3 урока (#6)
    * Вынесен хард код в созданных макетах.
    
    * Создан отдельный фрагмент "MainScreenFragment" со стартовым меню.
    Исперавлен фрагмент "PharmacyFragment" на "MainScreenFragment".

--- screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/navigator/AppScreensImpl.kt
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/navigator/AppScreensImpl.kt
@@ -2,14 +2,14 @@ package com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator
 
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.about.AboutFragment
-import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.PharmacyFragment
+import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.mainscreen.MainScreenFragment
 import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.PharmacySurfaceCalculateFragment
 import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.PharmacySurfaceCalculateResultFragment
 import com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.PharmacySurfaceFragment
 
 class AppScreensImpl: AppScreens {
     override fun pharmacyScreen() = FragmentScreen {
-        PharmacyFragment.newInstance()
+        MainScreenFragment.newInstance()
     }
 
     override fun pharmacySurfaceScreen() = FragmentScreen {
