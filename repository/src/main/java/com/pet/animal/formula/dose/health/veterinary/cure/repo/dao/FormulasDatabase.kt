615febcbd24a53480055507bf8f9fd1dce70bfc6
andreysc1976 <andreysc@yandex.ru>
2022-04-27 22:28:06 +0300

    добавлена зависимость room в модуль репозитория и сделан временный type converter

--- repository/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/repo/dao/FormulasDatabase.kt
+++ repository/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/repo/dao/FormulasDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.pet.animal.formula.dose.health.veterinary.cure.repo.FormulaEntity
 
 @Database (entities = [FormulaEntity::class], version = 1)
+@TypeConverters(ClassTypeConverter::class)
 abstract class FormulasDatabase: RoomDatabase() {
 
     abstract fun vetFormulaDao():VetFormulaDao
