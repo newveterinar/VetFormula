ecfef467f129a7f3b89c8211ed4322b5d54757c6
Shaparapatah <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>
2022-05-12 22:16:07 +0300

    Создал макет PharmacyDosesFragment.kt

---
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/pharmacy/doses/PharmacyDosesFragment.kt
@@ -0,0 +1,8 @@
+package com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.doses
+
+import com.pet.animal.formula.dose.health.veterinary.cure.core.base.BaseFragment
+import com.pet.animal.formula.dose.health.veterinary.cure.screens.databinding.FragmentPharmacyDosesBinding
+
+class PharmacyDosesFragment :
+    BaseFragment<FragmentPharmacyDosesBinding>(FragmentPharmacyDosesBinding::inflate) {
+}
