3b37b8d67de32901bbf62a82dfe94f0e87a702fd
Shaparapatah <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>
2022-05-13 12:36:51 +0300

    Создал макет PharmacyCRIFragment.kt

---
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/pharmacy/cri/PharmacyCRIFragment.kt
@@ -0,0 +1,8 @@
+package com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.pharmacy.cri
+
+import com.pet.animal.formula.dose.health.veterinary.cure.core.base.BaseFragment
+import com.pet.animal.formula.dose.health.veterinary.cure.screens.databinding.FragmentPharmacyCriBinding
+
+class PharmacyCRIFragment :
+    BaseFragment<FragmentPharmacyCriBinding>(FragmentPharmacyCriBinding::inflate) {
+}
