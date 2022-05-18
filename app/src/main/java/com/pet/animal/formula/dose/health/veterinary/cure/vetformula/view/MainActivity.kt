11da76c8cb501124a76615ade70dea5365a377a8
Shaparapatah <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>
2022-05-18 15:41:42 +0300

    В MainActivity.kt прописал FAB

--- app/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/vetformula/view/MainActivity.kt
+++ app/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/vetformula/view/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.pet.animal.formula.dose.health.veterinary.cure.vetformula.view
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
 import com.github.terrakok.cicerone.NavigatorHolder
-import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.pet.animal.formula.dose.health.veterinary.cure.core.calculator.CalcInteractorImpl
-import com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator.AppScreensImpl
 import com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator.BackButtonListener
 import com.pet.animal.formula.dose.health.veterinary.cure.utils.MAIN_ACTIVITY_NAME
 import com.pet.animal.formula.dose.health.veterinary.cure.vetformula.R
@@ -16,20 +17,26 @@ import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 import org.koin.java.KoinJavaComponent
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     /** Задание переменных */ //region
-        // Навигация
+    // Переменная для FAB
+    private var isExpanded = false
+
+    // Навигация
     private val navigator =
         AppNavigator(this@MainActivity, R.id.activity_fragments_container)
     private val navigatorHolder: NavigatorHolder = KoinJavaComponent.getKoin().get()
-        // ViewModel
+
+    // ViewModel
     private val mainActivityScope: Scope = KoinJavaComponent.getKoin().getOrCreateScope(
         MAIN_ACTIVITY_NAME, named(MAIN_ACTIVITY_NAME)
     )
     private lateinit var model: MainViewModel
-        // Binding
+
+    // Binding
     private lateinit var binding: ActivityMainBinding
-        // Калькулятор (интерактор для работы с калькулятором)
+
+    // Калькулятор (интерактор для работы с калькулятором)
     private val calcInteractor: CalcInteractorImpl = CalcInteractorImpl()
     //endregion
 
@@ -47,6 +54,8 @@ class MainActivity: AppCompatActivity() {
         if (savedInstanceState == null) {
             model.router.navigateTo(model.screens.mainScreen())
         }
+
+        setFAB()
     }
 
     /** Методы для настройки навигатора */ //region
@@ -55,11 +64,13 @@ class MainActivity: AppCompatActivity() {
         // Установка навигатора
         navigatorHolder.setNavigator(navigator)
     }
+
     override fun onPause() {
         super.onPause()
         // Удаление навигатора
         navigatorHolder.removeNavigator()
     }
+
     override fun onBackPressed() {
         supportFragmentManager.fragments.forEach {
             if (it is BackButtonListener && it.backPressed()) {
@@ -68,8 +79,100 @@ class MainActivity: AppCompatActivity() {
         }
         model.router.exit()
     }
-    //endregion
 
+    /** FAB **/
+    //region
+    private fun setFAB() {
+        setInitialState()
+
+        binding.fab.setOnClickListener {
+            if (isExpanded) {
+                collapseFAB()
+            } else {
+                expandFAB()
+            }
+        }
+    }
+
+    private fun setInitialState() {
+        binding.activityFragmentsContainer.apply {
+            alpha = 0f
+        }
+        binding.optionOneContainer.apply {
+            alpha = 0f
+            isClickable = false
+        }
+        binding.optionTwoContainer.apply {
+            alpha = 0f
+            isClickable = false
+        }
+    }
+
+    private fun expandFAB() {
+        isExpanded = true
+        ObjectAnimator.ofFloat(binding.fabImageview, "rotation", 0f, 255f).start()
+        ObjectAnimator.ofFloat(binding.optionOneContainer, "translationY", -130f).start()
+        ObjectAnimator.ofFloat(binding.optionTwoContainer, "translationY", -250f).start()
+
+        binding.optionTwoContainer.animate()
+            .alpha(1f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.optionTwoContainer.isClickable = true
+                    binding.optionTwoContainer.setOnClickListener {
+                        Toast.makeText(this@MainActivity, "Option 2", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+        binding.optionOneContainer.animate()
+            .alpha(1f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    binding.optionOneContainer.isClickable = true
+                    binding.optionOneContainer.setOnClickListener {
+                        Toast.makeText(this@MainActivity, "Option 1", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+    }
+
+    private fun collapseFAB() {
+        isExpanded = false
+        ObjectAnimator.ofFloat(binding.fabImageview, "rotation", 0f, -180f).start()
+        ObjectAnimator.ofFloat(binding.optionOneContainer, "translationY", 0f).start()
+        ObjectAnimator.ofFloat(binding.optionTwoContainer, "translationY", 0f).start()
+
+        binding.optionTwoContainer.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.optionTwoContainer.isClickable = false
+                    binding.optionTwoContainer.setOnClickListener(null)
+                }
+            })
+        binding.optionOneContainer.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.optionOneContainer.isClickable = false
+                }
+            })
+        binding.activityFragmentsContainer.animate()
+            .alpha(0f)
+            .setDuration(300)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.activityFragmentsContainer.isClickable = false
+                }
+            })
+
+    }
+
+    //endregion
     override fun onDestroy() {
         // Удаление скоупа для данной активити
         mainActivityScope.close()
