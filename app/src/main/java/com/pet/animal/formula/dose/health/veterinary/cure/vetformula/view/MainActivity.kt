11492180e846a2a227c4d6cd801078501c607ebf
Shaparapatah <61584086+Shaparapatah@users.noreply.github.com>
2022-05-19 19:11:32 +0300

    1. Убрал settings наверх.2. Убрал кнопку "О приложении" со всех экранов. (#43)
    * Вынес кнопку Settings в верхнее меню MainScreen
    
    * В MainActivity.kt прописал FAB
    
    * FAB работает, но MainActivity закрывает собой все экраны
    
    * Пока убрал FAB из приложения
    
    Co-authored-by: Shaparapatah <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>

--- app/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/vetformula/view/MainActivity.kt
+++ app/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/vetformula/view/MainActivity.kt
@@ -2,12 +2,9 @@ package com.pet.animal.formula.dose.health.veterinary.cure.vetformula.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
 import com.github.terrakok.cicerone.NavigatorHolder
-import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.pet.animal.formula.dose.health.veterinary.cure.core.calculator.CalcInteractorImpl
-import com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator.AppScreensImpl
 import com.pet.animal.formula.dose.health.veterinary.cure.screens.navigator.BackButtonListener
 import com.pet.animal.formula.dose.health.veterinary.cure.utils.MAIN_ACTIVITY_NAME
 import com.pet.animal.formula.dose.health.veterinary.cure.vetformula.R
@@ -16,36 +13,40 @@ import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 import org.koin.java.KoinJavaComponent
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     /** Задание переменных */ //region
-        // Навигация
+
+    // Навигация
     private val navigator =
         AppNavigator(this@MainActivity, R.id.activity_fragments_container)
     private val navigatorHolder: NavigatorHolder = KoinJavaComponent.getKoin().get()
-        // ViewModel
+
+    // ViewModel
     private val mainActivityScope: Scope = KoinJavaComponent.getKoin().getOrCreateScope(
         MAIN_ACTIVITY_NAME, named(MAIN_ACTIVITY_NAME)
     )
-    private lateinit var model: MainViewModel
-        // Binding
+    private lateinit var viewModel: MainViewModel
+
+    // Binding
     private lateinit var binding: ActivityMainBinding
-        // Калькулятор (интерактор для работы с калькулятором)
+
+    // Калькулятор (интерактор для работы с калькулятором)
     private val calcInteractor: CalcInteractorImpl = CalcInteractorImpl()
     //endregion
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         // Подключение Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         // Создание Scope для MainActivity
         val viewModel: MainViewModel by mainActivityScope.inject()
-        model = viewModel
+        this.viewModel = viewModel
 
         if (savedInstanceState == null) {
-            model.router.navigateTo(model.screens.mainScreen())
+            this.viewModel.router.navigateTo(this.viewModel.screens.mainScreen())
         }
     }
 
@@ -55,21 +56,23 @@ class MainActivity: AppCompatActivity() {
         // Установка навигатора
         navigatorHolder.setNavigator(navigator)
     }
+
     override fun onPause() {
         super.onPause()
         // Удаление навигатора
         navigatorHolder.removeNavigator()
     }
+
     override fun onBackPressed() {
         supportFragmentManager.fragments.forEach {
             if (it is BackButtonListener && it.backPressed()) {
                 return
             }
         }
-        model.router.exit()
+        viewModel.router.exit()
     }
-    //endregion
 
+    //endregion
     override fun onDestroy() {
         // Удаление скоупа для данной активити
         mainActivityScope.close()
