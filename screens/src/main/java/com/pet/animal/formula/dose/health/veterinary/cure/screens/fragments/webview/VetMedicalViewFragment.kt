5551b393201c1f7e936b338f6060b6a17da38558
Shaparapatah <61584086+Shaparapatah@users.noreply.github.com>
2022-06-09 16:17:09 +0300

    1.Сократил код FAB в MainActivity до одной функции(метода)2. Сделал ProgressBar для отображения загрузки сайтов WebView_WSAVA & WebView_VetMedical (#76)
    * Сделал цвет кнопки в тон остальным
    
    * Сократил немного код
    
    * 1.Сократил код FAB в MainActivity до одной функции(метода)
    2. Сделал ProgressBar для отображения загрузки сайтов WebView_WSAVA & WebView_VetMedical
    
    * 23. Отделить названия разделов так, чтобы они не залезали друг на друга в окне "MainScreen"
    
    Co-authored-by: Shaparapatah <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>
    Co-authored-by: AVKhakhalin <78661461+AVKhakhalin@users.noreply.github.com>

--- screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/webview/VetMedicalViewFragment.kt
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/webview/VetMedicalViewFragment.kt
@@ -1,8 +1,10 @@
 package com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.webview
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.lifecycle.ViewModelProvider
 import com.pet.animal.formula.dose.health.veterinary.cure.core.base.BaseFragment
@@ -11,26 +13,40 @@ import com.pet.animal.formula.dose.health.veterinary.cure.screens.databinding.Fr
 class VetMedicalViewFragment :
     BaseFragment<FragmentWebViewVetmedicalBinding>(FragmentWebViewVetmedicalBinding::inflate) {
 
+    private val loadUrl = "https://vetmedical.ru/"
     private lateinit var viewModel: VetMedicalViewFragmentViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initViewModel()
-
         webViewSetup()
     }
 
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun webViewSetup() {
-        binding.webViewFragmentVetmedical.webViewClient = WebViewClient()
-        binding.webViewFragmentVetmedical.apply {
-            loadUrl("https://vetmedical.ru/")
-            settings.javaScriptEnabled = true
-            settings.allowContentAccess = true
+        with(binding) {
+            webViewFragmentVetmedical.webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    view?.visibility = View.INVISIBLE
+                    progressbarVetmedical.visibility = View.VISIBLE
+                }
 
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    view?.visibility = View.VISIBLE
+                    progressbarVetmedical.visibility = View.INVISIBLE
+                }
+            }
+            webViewFragmentVetmedical.apply {
+                loadUrl(loadUrl)
+                settings.javaScriptEnabled = true
+                settings.allowContentAccess = true
+            }
         }
+
     }
 
 
