dc59cc933bc83ae1b18f7f509346e01aaa165bd6
KPAHAPK <Roman444.zaharov@gmail.com>
2022-06-10 21:28:15 +0300

    Подсказки при долгом нажатии на настройки и о приложении

--- screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/mainscreen/MainScreenFragmentViewModel.kt
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/mainscreen/MainScreenFragmentViewModel.kt
@@ -1,6 +1,16 @@
 package com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.mainscreen
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.pet.animal.formula.dose.health.veterinary.cure.core.base.BaseViewModelForNavigation
 
 class MainScreenFragmentViewModel: BaseViewModelForNavigation() {
+
+    private val _toastHint = MutableLiveData<String>()
+    val toastHint: LiveData<String>
+        get() = _toastHint
+
+    fun setToastHint(hint: String) {
+        _toastHint.value = hint
+    }
 }
