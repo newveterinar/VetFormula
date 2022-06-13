e17136e964697c6eb49c46a056bc466691f6d137
KPAHAPK <Roman444.zaharov@gmail.com>
2022-06-13 20:23:59 +0300

    Выводится диалог сохранения и сохраняется одно значение

--- screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/edittext/EditTextFragment.kt
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/edittext/EditTextFragment.kt
@@ -1,11 +1,14 @@
 package com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.edittext
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import com.pet.animal.formula.dose.health.veterinary.cure.core.base.BaseFragment
+import com.pet.animal.formula.dose.health.veterinary.cure.screens.R
 import com.pet.animal.formula.dose.health.veterinary.cure.screens.databinding.FragmentEditTextBinding
 import com.pet.animal.formula.dose.health.veterinary.cure.utils.FragmentScope
 import com.pet.animal.formula.dose.health.veterinary.cure.utils.settings.SettingsImpl
@@ -13,15 +16,20 @@ import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 import org.koin.java.KoinJavaComponent
 
-class EditTextFragment: BaseFragment<FragmentEditTextBinding>(FragmentEditTextBinding::inflate) {
+class EditTextFragment : BaseFragment<FragmentEditTextBinding>(FragmentEditTextBinding::inflate) {
     /** Задание переменных */ //region
     private lateinit var viewModel: EditTextFragmentViewModel
+
     // Текстовый элемент
     lateinit var editText: TextView
+    private var StringBuilder = ""
+
     // SettingsImpl
     private val settings: SettingsImpl = KoinJavaComponent.getKoin().get()
+
     // ShowPharmacySurfaceResultFragmentScope
     private lateinit var showEditTextFragmentScope: Scope
+
     // newInstance для данного класса
     companion object {
         fun newInstance(): EditTextFragment = EditTextFragment()
@@ -37,6 +45,7 @@ class EditTextFragment: BaseFragment<FragmentEditTextBinding>(FragmentEditTextBi
             named(FragmentScope.SHOW_EDIT_TEXT_FRAGMENT_SCOPE)
         )
     }
+
     override fun onDetach() {
         // Удаление скоупа для данного фрагмента
         showEditTextFragmentScope.close()
@@ -50,13 +59,51 @@ class EditTextFragment: BaseFragment<FragmentEditTextBinding>(FragmentEditTextBi
         initEditText()
         // Инициализация ViewModel
         initViewModel()
+        initButtons()
+
+        viewModel.urlLiveData.observe(viewLifecycleOwner) { pair ->
+            val builder = AlertDialog.Builder(requireContext())
+            val inflater = layoutInflater
+            val dialogView = inflater.inflate(R.layout.dialog_save, null)
+            builder.setView(dialogView)
+            val dialog = builder.create()
+            val btnSaveWasa = dialogView.findViewById<Button>(R.id.btn_save_wasa)
+            val btnSaveVetmedical =
+                dialogView.findViewById<Button>(R.id.btn_save_vetmedical)
+            val btnSaveCancle = dialogView.findViewById<Button>(R.id.btn_save_cancle)
+            btnSaveVetmedical.setOnClickListener {
+                binding.editTextText.setText(viewModel.urlLiveData.value?.second)
+                dialog.dismiss()
+            }
+            btnSaveWasa.setOnClickListener {
+                binding.editTextText.setText(viewModel.urlLiveData.value?.first)
+                dialog.dismiss()
+            }
+            btnSaveCancle.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.show()
+        }
+    }
+
+    private fun initButtons() {
+        binding.apply {
+            btnSaveNote.setOnClickListener {
+                viewModel.setUrlLiveData(Pair(settings.getCurrentVetmedicalUrl(),
+                    settings.getCurrentWsavaUrl()))
+            }
+
+            btnLoadNote.setOnClickListener {
+
+            }
+        }
     }
 
     // Инициализация текстового элемента
     @SuppressLint("SetTextI18n")
     private fun initEditText() {
         editText = binding.editTextText
-        editText.text = "${settings.getCurrentVetmedicalUrl()}\n${settings.getCurrentWsavaUrl()}"
+        //editText.text = "${settings.getCurrentVetmedicalUrl()}\n${settings.getCurrentWsavaUrl()}"
     }
 
     // Инициализация ViewModel
--- screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/edittext/EditTextFragmentViewModel.kt
+++ screens/src/main/java/com/pet/animal/formula/dose/health/veterinary/cure/screens/fragments/edittext/EditTextFragmentViewModel.kt
@@ -1,6 +1,22 @@
 package com.pet.animal.formula.dose.health.veterinary.cure.screens.fragments.edittext
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.pet.animal.formula.dose.health.veterinary.cure.core.base.BaseViewModelForNavigation
+import java.lang.StringBuilder
 
 class EditTextFragmentViewModel : BaseViewModelForNavigation() {
+
+    var stringBuilder = StringBuilder()
+
+    private val _urlLiveData = MutableLiveData<Pair<String, String>>()
+    val urlLiveData: LiveData<Pair<String, String>> = _urlLiveData
+
+    fun setUrlLiveData(pair: Pair<String, String>) {
+        _urlLiveData.value = pair
+    }
+
+    fun addUrlAsNote(string: String){
+        stringBuilder.append(string)
+    }
 }
